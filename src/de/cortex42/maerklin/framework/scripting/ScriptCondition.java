271bb5f7d75f3e5f1a63d769c4d007dd08381f2a
cortex42 <ivo.palazzolo@gmx.de>
2016-01-12 17:04:12 +0100

    innerScriptCondition

--- src/de/cortex42/maerklin/framework/scripting/ScriptCondition.java
+++ src/de/cortex42/maerklin/framework/scripting/ScriptCondition.java
@@ -7,29 +7,50 @@ import de.cortex42.maerklin.framework.FrameworkException;
  */
 public class ScriptCondition {
     private final BooleanEvent booleanEvent;
+    private final ScriptCondition innerScriptCondition;
     private ScriptCondition otherScriptCondition = null;
     private boolean isOr = false;
     private boolean isAnd = false;
     private boolean isXor = false;
 
-    public ScriptCondition(final BooleanEvent booleanEvent) { //todo ScriptCondition as argument?
+    public ScriptCondition(final ScriptCondition scriptCondition) { //todo check ALL parameters for null!
+        if (scriptCondition == null) {
+            throw new IllegalArgumentException("scriptCondition must not be null.");
+        }
+
+        this.innerScriptCondition = scriptCondition;
+        this.booleanEvent = null;
+    }
+
+    public ScriptCondition(final BooleanEvent booleanEvent) {
+        if (booleanEvent == null) {
+            throw new IllegalArgumentException("booleanEvent must not be null.");
+        }
+
         this.booleanEvent = booleanEvent;
+        this.innerScriptCondition = null;
     }
 
     public boolean check() throws FrameworkException {
+        boolean returnValue;
+
+        if (innerScriptCondition != null) {
+            returnValue = innerScriptCondition.check();
+        } else {
+            returnValue = booleanEvent.getAsBoolean();
+        }
+
         if (otherScriptCondition != null) {
             if (isOr) {
-                return booleanEvent.getAsBoolean() || otherScriptCondition.check();
+                returnValue = returnValue || otherScriptCondition.check();
             } else if (isAnd) {
-                return booleanEvent.getAsBoolean() && otherScriptCondition.check();
-            } else if (isXor) {
-                return booleanEvent.getAsBoolean() ^ otherScriptCondition.check();
+                returnValue = returnValue && otherScriptCondition.check();
             } else {
-                throw new ScriptConditionBooleanOperatorException("No operation was set.");
+                returnValue = returnValue ^ otherScriptCondition.check();
             }
         }
 
-        return booleanEvent.getAsBoolean();
+        return returnValue;
     }
 
     public ScriptCondition or(final ScriptCondition scriptCondition) throws ScriptConditionBooleanOperatorException {
@@ -37,7 +58,7 @@ public class ScriptCondition {
             otherScriptCondition = scriptCondition;
             isOr = true;
 
-            return this;
+            return this; //todo needed??
         } else {
             throw new ScriptConditionBooleanOperatorException("Operator was already set.");
         }
